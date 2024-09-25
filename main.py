d542744dc28197103940dd833596069b21b7d396
WifiLast <lordofdoenner@gmail.com>
2024-09-25 14:38:53 +0200

    added ssl config

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+import os,sys
+from shutil import which
 from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
 
 class Window(Frame):
 
+    def openNewWindow(self):
+     
+        # Toplevel object which will 
+        # be treated as a new window
+        newWindow = Toplevel(root)
+
+        # sets the title of the
+        # Toplevel widget
+        newWindow.title("Settings")
+
+        # sets the geometry of toplevel
+        newWindow.geometry("200x800")
+
+        # A Label widget to show in toplevel
+        Label(newWindow, 
+                text ="This is a new window").pack()
+
+
+    def check_openssl():
+        return which("openssl")
+
+    def create_cert(self):
+        os.system("openssl genrsa -out key.pem 2048 ")
+        os.system("openssl req -x509 -days 365 -new -out certificate.pem -key key.pem -config ssl.conf")
+
     def __init__(self, master=None):
-        Frame.__init__(self, master)        
+        Frame.__init__(self, master)   
+        print ("Main frame started")  
         self.master = master
 
 
@@ -23,16 +54,18 @@ class Window(Frame):
         self.pack(fill=BOTH, expand=1)
 
         # create button, link it to clickExitButton()
-        exitButton = Button(self, text="Exit", command=self.clickExitButton)
+        certButton = Button(self, text="Create Certificate", command=self.create_cert)
+        if(self.check_openssl == None):
+            certButton["state"] = "disabled"
+        certButton.place(x=5, y=5)
 
-        # place button at (0,0)
-        exitButton.place(x=5, y=5)
+        settings = Button(self, text="New window", command=self.openNewWindow)
+        settings.place(x=5, y=50)        
 
     def clickExitButton(self):
         exit()
         
-root = Tk()
 app = Window(root)
 root.wm_title("Tkinter button")
-root.geometry("320x200")
+root.geometry("800x600")
 root.mainloop()
